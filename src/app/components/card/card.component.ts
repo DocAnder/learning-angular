8e670e08e398500f81cbacf9b6a7edfcfc75dfee
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-09-24 09:39:56 -0300

    create a card component

---
+++ src/app/components/card/card.component.ts
@@ -0,0 +1,26 @@
+import { Component, EventEmitter, Input, Output } from '@angular/core';
+
+@Component({
+  selector: 'app-card',
+  templateUrl: './card.component.html',
+  styleUrl: './card.component.scss'
+})
+export class CardComponent {
+
+  @Input({required: true})
+  planType: string = '';
+
+  @Input({required:true})
+  planPrice: number = 0;
+
+  @Input({required:true})
+  cardStyle: 'orange' | 'purple' = 'orange'
+
+  @Output('buttonCliked')
+  buttonClikedEmmit = new EventEmitter<void>();
+
+  onButtonClicked() {
+    this.buttonClikedEmmit.emit();
+  }
+
+}
