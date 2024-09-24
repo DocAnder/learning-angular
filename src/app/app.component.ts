de62d0b742da1eda59794730bfb57acadcd8b28b
Anderson Oliveira <andersonmartinseoliveira@gmail.com>
2024-09-24 11:11:01 -0300

    define prop tu bind on ngIf and set some truthy and false possibilities

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,12 +6,36 @@ import { Component } from '@angular/core';
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
-  name: string = 'Anderson';
+  prop = true;
 
+  buttonToggle(){
+    this.prop = !this.prop;
+  }
 
-  // handleInputChange(event: any){
-  //   console.log(event)
-  //   this.name = event;
-  // }
+  //Retornos que serão verdadeiros
+  isTruthy(){
+    // return {};
+    // return [];
+    // return true;
+    // return 1;
+    // return "teste";
+    // return "0";
+    // return "false";
+    // return this.pessoa.status;
+  }
+
+  //Retornos que serão falsos
+  isFalse(){
+    // return false;
+    // return 0;
+    // return "";
+    // return '';
+    // return null;
+    // return undefined;
+    // return -0;
+    // return NaN;
+    // return this.pessoa.name;
+  }
 
 }
+ 
